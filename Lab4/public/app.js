8d09669db97928a0fbdb1135d607c8ef7061dab4
Radwa Hassan <63107268+RadwaHassan99@users.noreply.github.com>
2023-03-07 10:52:44 +0200

    chat Lab4

---
+++ Lab4/public/app.js
@@ -0,0 +1,57 @@
+const socket = io();
+const msgerForm = get(".msger-form"); 
+const msgerInput = get(".msger-input");
+const msgerChat = get(".msger-chat");
+const userId = Math.floor(Math.random() * 10000);
+
+const user1_NAME = "";
+const user2_NAME = "";
+msgerForm.addEventListener("submit", event => {
+  event.preventDefault();
+  const msgText = msgerInput.value;
+  if (!msgText) return;
+  socket.emit('chat message',  {userId,msgText});
+  msgerInput.value = "";
+});
+
+socket.on('chat message', function (data) {
+    if (data.userId === userId) {
+        appendMessage(user1_NAME,"right", data.msgText);
+      } else {
+        appendMessage(user2_NAME,"left", data.msgText);
+      }    
+});
+
+function appendMessage(name, side, text) {
+  const msgHTML = `
+    <div class="msg ${side}-msg">
+
+      <div class="msg-bubble">
+        <div class="msg-info">
+          <div class="msg-info-name">${name}</div>
+          <div class="msg-info-time">${formatDate(new Date())}</div>
+        </div>
+
+        <div class="msg-text">${text}</div>
+      </div>
+    </div>
+  `;
+
+  msgerChat.insertAdjacentHTML("beforeend", msgHTML);
+  msgerChat.scrollTop += 500;
+}
+
+function get(selector, root = document) {
+  return root.querySelector(selector);
+}
+
+function formatDate(date) {
+  const h = "0" + date.getHours();
+  const m = "0" + date.getMinutes();
+
+  return `${h.slice(-2)}:${m.slice(-2)}`;
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
